076093e7bb5998c229f4bddc05e55c3d93c791ad
simiseee <simiseee@gmail.com>
2019-06-09 18:48:37 +0300

    bubble sort

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 fn main() {
     let mut vector: Vec<i32> = vec![1, 2, 33, 2, 4, 1, 3, 5, 9, 11, 7, 99];
 
-    selection_sorting(&vector);
-    linear_search(&vector, 5);
+    // selection_sorting(&vector);
+    // linear_search(&vector, 5);
+    bubble_sort(&vector);
 }
 
 // Sort vector by selection
@@ -41,4 +42,33 @@ fn linear_search(vector: &Vec<i32>, num: i32) {
 
     println!("Didn't found number: {}", &num);
     return;
+}
+
+// Sort vector by "Bubble sort"
+fn bubble_sort(vector: &Vec<i32>) {
+    let mut vector = vector.clone();
+    let length = vector.len();
+    let swap = true;
+
+    while swap {
+        let mut swap_count = 0;
+
+        for i in 1..length {
+            if vector[i - 1] > vector[i] {
+                let temp = vector[i];
+
+                vector[i] = vector[i - 1];
+
+                vector[i - 1] = temp;
+
+                swap_count += 1;
+            }
+        }
+
+        if swap_count == 0 {
+            break;
+        }
+    }
+
+    println!("Bubble sorted vector: {:?}", vector);
 }
